13ab3d6a2eb5bd4c19092dec32d303ea1289ca6a
mushfiqulIslam <mushfiqul.islam@field.buzz>
2021-03-11 01:38:11 +0600

    updated train_val_test_spilter and added settings example

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # [Real Time Action Recognition From Video Footage](https://github.com/mushfiqulIslam/real-time-action-recognition-from-video-footage)
 
 ## Project Setup
-Download [Dataset](https://drive.google.com/drive/folders/1BTUFlf-5CBbTuCmE6mEv3CBD1TjGEhkU?usp=sharing) and move inside the directory. Update train_val_test_spliter files path variables
+Download [Dataset](https://drive.google.com/drive/folders/1BTUFlf-5CBbTuCmE6mEv3CBD1TjGEhkU?usp=sharing) and move inside the directory. Create a file like settings.py.example and update path variables
 Create env and run following commands
 - conda install --file conda_req.txt
 - pip install -r requirements.txt
--- tools/train_val_test_spliter.py
+++ tools/train_val_test_spliter.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
 import random
 import os
 import pandas as pd
+from tools.settings import *
 '''
     This script will save 3 files: train.txt, val.txt, test.txt.
     each file has the following format:
     --------------------------------------------------------------
-    PathFileNameOfFile <Tab> ActionName
+    PathFile <Tab> actionName
     ex:
         data/video/video_1	slap
         data/video/video_2	punch
@@ -14,70 +14,62 @@ import pandas as pd
 
 	--------------------------------------------------------------
 '''
+punch_path = punch_path
+kick_path = kick_path
 
-LIST_OF_SLAP_DIR = ["/home/mushfiqul/Mushfiqul/CSE/Thesis2.0/real-time-action-recognition-from-video-footage/dataset/slap/"]
-LIST_OF_PUNCH_DIR = ["/home/mushfiqul/Mushfiqul/CSE/Thesis2.0/real-time-action-recognition-from-video-footage/dataset/punch/"]
-LIST_OF_KICK_DIR = ["/home/mushfiqul/Mushfiqul/CSE/Thesis2.0/real-time-action-recognition-from-video-footage/dataset/kick/"]
+def accumulate_videos(videos_path):
+    videos = os.listdir(videos_path)
+    videos_path_list = []
+    for video in videos:
+        videos_path_list.append(videos_path + video)
+    return videos_path_list
 
-VAL_SET_RATIO = 0.1
-TEST_SET_RATIO = 0.4   # NUMBER_OF_TEST_SET = NUMBER_OF_TOTAL_DATA * TEST_SET_RATIO
+def append_label(videos_path, action):
+    data_with_label = []
+    for video_path in videos_path:
+        data_with_label.append([video_path, action])
+    return data_with_label
 
-PATH_TO_SAVE_SPLITED_DATASET = "/home/mushfiqul/Mushfiqul/CSE/Thesis2.0/real-time-action-recognition-from-video-footage/dataset"
+def split_train_val_test_data(videos):
+    random.shuffle(videos)
+    number_of_total_data = len(videos)
+    number_of_test_videos = int(number_of_total_data * test_dataset_ratio)
+    number_of_val_videos = int(number_of_total_data * val_dataset_ratio)
 
-def AccumulateAllVideoFromDifferentDir(LIST_OF_DIR_THAT_CONTAINS_VIDEOS_):
-    listOfPathToVideos = []
-    for eachDir in LIST_OF_DIR_THAT_CONTAINS_VIDEOS_:
-        listOfVideos = os.listdir(eachDir)
-        for eachVideo in listOfVideos:
-            listOfPathToVideos.append(eachDir + eachVideo)
-    return listOfPathToVideos
-
-def AppendLabelToEachData(LIST_OF_DATA_, action_):
-    listOfDataWithLabel = []
-    for eachData in LIST_OF_DATA_:
-        listOfDataWithLabel.append([eachData, action_])
-    return listOfDataWithLabel
-
-def Split_Train_Val_Test_Data(LIST_OF_VIDEOS_):
-    random.shuffle(LIST_OF_VIDEOS_)
-    NUMBER_OF_TOTAL_DATA = len(LIST_OF_VIDEOS_)
-    NUMBER_OF_TEST_VIDEOS = int(NUMBER_OF_TOTAL_DATA * TEST_SET_RATIO)
-    NUMBER_OF_VAL_VIDEOS = int(NUMBER_OF_TOTAL_DATA * VAL_SET_RATIO)
-
-    listOfTestVideos = LIST_OF_VIDEOS_[ : NUMBER_OF_TEST_VIDEOS]
-    listOfValVideos = LIST_OF_VIDEOS_[NUMBER_OF_TEST_VIDEOS : (NUMBER_OF_TEST_VIDEOS+NUMBER_OF_VAL_VIDEOS)]
-    listOfTrainVideos = LIST_OF_VIDEOS_[(NUMBER_OF_TEST_VIDEOS+NUMBER_OF_VAL_VIDEOS) : ]
-    return listOfTrainVideos, listOfValVideos, listOfTestVideos
+    list_of_test_videos = videos[ : number_of_test_videos]
+    list_of_val_videos = videos[number_of_test_videos : (number_of_test_videos+number_of_val_videos)]
+    list_of_train_videos = videos[(number_of_test_videos+number_of_val_videos) : ]
+    return list_of_train_videos, list_of_val_videos, list_of_test_videos
 
 def split():
-    print("Splitting the ginen dataset into Train Test={0} Validation={1}".format(TEST_SET_RATIO, VAL_SET_RATIO))
+    print("Splitting the ginen dataset into Train Test={0} Validation={1}".format(test_dataset_ratio, val_dataset_ratio))
     #Preparing Dataset List
     #slap
-    listofSlapVideos = AccumulateAllVideoFromDifferentDir(LIST_OF_SLAP_DIR)
-    listOfSlapVideos = AppendLabelToEachData(listofSlapVideos, "slap")
-    trainSlapVideos, valSlapVideos, testSlapVideos = Split_Train_Val_Test_Data(listOfSlapVideos)
+    slap_videos = accumulate_videos(slap_path)
+    slap_videos = append_label(slap_videos, "slap")
+    train_slap_videos, val_slap_videos, test_slap_videos = split_train_val_test_data(slap_videos)
     #punch
-    listofPunchVideos = AccumulateAllVideoFromDifferentDir(LIST_OF_PUNCH_DIR)
-    listofPunchVideos = AppendLabelToEachData(listofPunchVideos, "punch")
-    trainPunchVideos, valPunchVideos, testPunchVideos = Split_Train_Val_Test_Data(listofPunchVideos)
+    punch_videos = accumulate_videos(punch_path)
+    punch_videos = append_label(punch_videos, "punch")
+    train_punch_videos, val_punch_videos, test_punch_videos = split_train_val_test_data(punch_videos)
     #kick
-    listofKickVideos = AccumulateAllVideoFromDifferentDir(LIST_OF_KICK_DIR)
-    listofKickVideos = AppendLabelToEachData(listofKickVideos, "kick")
-    trainKickVideos, valKickVideos, testKickVideos = Split_Train_Val_Test_Data(listofKickVideos)
+    kick_videos = accumulate_videos(kick_path)
+    kick_videos = append_label(kick_videos, "kick")
+    train_kick_videos, val_kick_videos, test_kick_videos = split_train_val_test_data(kick_videos)
 
     #Preparing Dataset
-    listOfTrainData = trainSlapVideos + trainPunchVideos + trainKickVideos
-    random.shuffle(listOfTrainData)
-    train = pd.DataFrame(listOfTrainData, columns = ['Video_url', 'Action'])
-    train.to_csv(os.path.join(PATH_TO_SAVE_SPLITED_DATASET, 'train.csv'))
+    train_data_list = train_slap_videos + train_punch_videos + train_kick_videos
+    random.shuffle(train_data_list)
+    train = pd.DataFrame(train_data_list, columns = ['Video_url', 'action'])
+    train.to_csv(os.path.join(dataset_path, 'train.csv'))
 
-    listOfValData = valSlapVideos + valPunchVideos + valKickVideos
-    random.shuffle(listOfValData)
-    val = pd.DataFrame(listOfValData, columns = ['Video_url', 'Action'])
-    val.to_csv(os.path.join(PATH_TO_SAVE_SPLITED_DATASET, 'val.csv'))
+    val_data_list = val_slap_videos + val_punch_videos + val_kick_videos
+    random.shuffle(val_data_list)
+    val = pd.DataFrame(val_data_list, columns = ['Video_url', 'action'])
+    val.to_csv(os.path.join(dataset_path, 'val.csv'))
 
-    listOfTestData = testSlapVideos + testPunchVideos + testKickVideos
-    random.shuffle(listOfTestData)
-    test = pd.DataFrame(listOfTestData, columns = ['Video_url', 'Action'])
-    test.to_csv(os.path.join(PATH_TO_SAVE_SPLITED_DATASET, 'test.csv'))
+    test_data_list = test_slap_videos + test_punch_videos + test_kick_videos
+    random.shuffle(test_data_list)
+    test = pd.DataFrame(test_data_list, columns = ['Video_url', 'action'])
+    test.to_csv(os.path.join(dataset_path, 'test.csv'))
     print("Done")
